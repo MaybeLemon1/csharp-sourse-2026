4d9ce84b654829750ae98bf7bb9646db37d29d96
MaybeLemon1 <maybelemon@naver.com>
2026-08-31 14:34:42 +0900

    260831_파일_조작_연습문제_Test

---
+++ Day03_Exception_File_Test/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using Day03_Exception_File;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
+namespace Day03_Exception_File_Test;
+
+public class Tests
+{
+
+    [Test]
+    public void No_txt_에_들어_있는_내용을_Yes_txt_에_붙여넣기()
+    {
+        File.WriteAllText("Yes.txt", "Yes");
+        File.WriteAllText("No.txt", "No");
+
+        DefaultFileCopier copier = new DefaultFileCopier();
+
+        copier.CopyFile("No.txt", "Yes.txt");
+
+        string result = File.ReadAllText("Yes.txt");
+
+        Assert.That(result, Is.EqualTo("YesNo"));
+        Console.WriteLine(result);
+    }
+}
