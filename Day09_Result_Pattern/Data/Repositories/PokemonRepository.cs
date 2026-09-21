0b23c5b01400fe7e2464a95a194ee5aea7c2c752
MaybeLemon1 <maybelemon@naver.com>
2026-09-21 11:22:10 +0900

    260921_Result_Pattern_과제1

---
+++ Day09_Result_Pattern/Data/Repositories/PokemonRepository.cs
@@ -0,0 +1,57 @@
+﻿using Day09_Result_Pattern.Data.DataSources;
+using Day09_Result_Pattern.Data.DTO;
+using Day09_Result_Pattern.Data.Models;
+using Day09_Result_Pattern.Data.Common;
+using Day09_Result_Pattern.Data.Common.Errors;
+using Day09_Result_Pattern.Data.Mapper;
+
+namespace Day09_Result_Pattern.Data.Repositores;
+
+public class PokemonRepository : IPokemonRepository
+{
+    private IPokemonApiDataSource _dataSource;
+
+    public PokemonRepository(IPokemonApiDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+
+    public async Task<Result<Pokemon, PokemonError>> GetPokemonByNameAsync(
+        string pokemonName)
+    {
+        try
+        {
+            Response<PokemonDto> response =
+                await _dataSource.GetPokemonAsync(pokemonName);
+
+            switch (response.StatusCode)
+            {
+                case 200:
+                    return new Result<Pokemon, PokemonError>.Success(
+                        response.Body.ToModel()
+                    );
+
+                case 404:
+                    return new Result<Pokemon, PokemonError>.Error(
+                        PokemonError.NotFound
+                    );
+
+                case -1:
+                    return new Result<Pokemon, PokemonError>.Error(
+                        PokemonError.NetworkTimeout
+                    );
+
+                default:
+                    return new Result<Pokemon, PokemonError>.Error(
+                        PokemonError.Unknown
+                    );
+            }
+        }
+        catch (Exception e)
+        {
+            return new Result<Pokemon, PokemonError>.Error(
+                PokemonError.Unknown
+            );
+        }
+    }
+}
---
+++ Day09_Result_Pattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using Day09_Result_Pattern.Data.DataSources;
+using Day09_Result_Pattern.Data.Models;
+using Day09_Result_Pattern.Data.Repositores;
+using Day09_Result_Pattern.Data.Common;
+using Day09_Result_Pattern.Data.Common.Errors;
+
+namespace Day09_Result_Pattern;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IPokemonApiDataSource dataSource = new PokemonApiDataSource(new HttpClient());
+        IPokemonRepository repository = new PokemonRepository(dataSource);
+
+        Result<Pokemon, PokemonError> result = await repository.GetPokemonByNameAsync("dittooo");
+        
+        switch (result)
+        {
+            case Result<Pokemon, PokemonError>.Success successResult:
+                Console.WriteLine($"포켓몬 이름: {successResult.data.Name}");
+                Console.WriteLine($"이미지URL: {successResult.data.ImageUrl}");
+                break;
+            case Result<Pokemon, PokemonError>.Error errorResult:
+                switch (errorResult.error)
+                {
+                    case PokemonError.NotFound:
+                        Console.WriteLine("오류: 해당 포켓몬을 찾을 수 없습니다.");
+                        break;
+                    case PokemonError.NetworkTimeout:
+                        Console.WriteLine("오류: 네트워크 연결 시간 초과.");
+                        break;
+                    default:
+                        Console.WriteLine("알 수 없는 오류가 발생했습니다.");
+                        break;
+                }
+                break;
+        }
+    }
+}
